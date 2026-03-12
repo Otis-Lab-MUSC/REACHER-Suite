8803b7bfbbe7a206ee9ecf6f74734b6102643601
thejoshbq <thejoshbq@proton.me>
2026-03-12 12:34:13 -0400

    Initial commit with site

---
+++ site/src/components/layout/CyberpunkGridBackground.tsx
@@ -0,0 +1,8 @@
+export default function CyberpunkGridBackground() {
+  return (
+    <div
+      className="cyberpunk-grid fixed inset-0 pointer-events-none z-0"
+      aria-hidden="true"
+    />
+  )
+}
